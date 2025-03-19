a503df84b8f4e423c42bdf263027742e8451ccc2
maria clara lopes <134817081+Martinlopes555@users.noreply.github.com>
2025-03-19 14:01:18 +0000

    Update Payment Controller

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
 import express from 'express';
 const server = express();
 
-import routerPayment from '.routes/payment.routes.js'
+import routerPayment from './routes/payment.routes.js'
 
 const PORT = process.env.PORT || 3000;
 
